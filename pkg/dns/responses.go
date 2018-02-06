44857f6b331d3c2f9a9d8c36dab30a8ff567500e
ennetech <neurogas@gmail.com>
2018-02-06 19:50:23 +0100

    Removed duplicated code from the responses

--- pkg/dns/responses.go
+++ pkg/dns/responses.go
@@ -2,42 +2,28 @@ package dns
 
 import "github.com/miekg/dns"
 
-func sendSuccess(w dns.ResponseWriter, r *dns.Msg, recordsAnswer []dns.RR) string {
+func sendMessage(w dns.ResponseWriter, r *dns.Msg, recordsAnswer []dns.RR, rcode int) dns.Msg {
 	a := new(dns.Msg)
 	a.SetReply(r)
 	a.Compress = true
 	a.Authoritative = true
 	a.Answer = recordsAnswer
-	a.Rcode = dns.RcodeSuccess
+	a.Rcode = rcode
 	w.WriteMsg(a)
-	return a.String()
+	return *a
+}
+
+func sendSuccess(w dns.ResponseWriter, r *dns.Msg, recordsAnswer []dns.RR) dns.Msg {
+	return sendMessage(w, r, recordsAnswer, dns.RcodeSuccess)
 }
 
 func sendNxDomain(w dns.ResponseWriter, r *dns.Msg) dns.Msg {
-	a := new(dns.Msg)
-	a.SetReply(r)
-	a.Compress = true
-	a.Authoritative = true
-	a.Rcode = dns.RcodeNameError
-	w.WriteMsg(a)
-	return *a
+	return sendMessage(w, r, []dns.RR{}, dns.RcodeNameError)
 }
 func sendNotImplemented(w dns.ResponseWriter, r *dns.Msg) dns.Msg {
-	a := new(dns.Msg)
-	a.SetReply(r)
-	a.Compress = true
-	a.Authoritative = true
-	a.Rcode = dns.RcodeNotImplemented
-	w.WriteMsg(a)
-	return *a
+	return sendMessage(w, r, []dns.RR{}, dns.RcodeNotImplemented)
 }
 
 func sendRefused(w dns.ResponseWriter, r *dns.Msg) dns.Msg {
-	a := new(dns.Msg)
-	a.SetReply(r)
-	a.Compress = true
-	a.Authoritative = true
-	a.Rcode = dns.RcodeRefused
-	w.WriteMsg(a)
-	return *a
+	return sendMessage(w, r, []dns.RR{}, dns.RcodeRefused)
 }
